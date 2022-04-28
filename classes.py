16dc57717a08a7fb9635e500a915206be553b5e9
michailidm <72384900+michailidm@users.noreply.github.com>
2022-04-28 22:09:29 +0300

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,107 @@
+class Game():
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def setup(self):
+        pass
+
+    def run(self):
+        """
+        menu = {}
+        menu['1'] = "Σκορ"
+        menu['2'] = "Ρυθμίσεις"
+        menu['3'] = "Παιχνίδι"
+        menu['q'] = "Έξοδος"
+
+
+        while True: 
+            options = menu.keys()
+            for entry in options: 
+                print(entry, ":", menu[entry])
+
+            selection = input("Please Select:") 
+            if selection == '1':
+                pass 
+                #print "add"
+            elif selection == '2':
+                pass 
+                #print "delete"
+            elif selection == '3':
+                pass
+                #print "find" 
+            elif selection == 'q':
+                pass 
+                #break
+            else:
+                pass 
+                #print "Unknown Option Selected!" 
+
+        """
+        print("***** Scrable *****")
+        print("-------------------")
+        print("1: Σκορ\n2: Ρυθμίσεις\n3: Παιχνίδι\nq: Έξοδος")
+        print("-------------------")
+        ans = input("Please choose from the menu: ")
+        
+
+    def end(self):
+        pass
+
+
+class SakClass():
+    def __init__(self, sak):
+        self.sak = sak
+        
+        
+    def randomize_sak(self):
+        pass
+
+    def getletters(self):
+        pass
+
+    def putbackletters(self):
+        pass
+
+class Player():
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+
+class Human(Player):
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def play(self):
+        pass
+
+
+class Computer(Player):
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+    
+    def play(self):
+        pass
+
+
+
+
+
+        
+    
+
+        
+
+        
---
+++ main-36082642.py
@@ -0,0 +1,16 @@
+import classes
+
+letters = {
+    "Α":"1",
+    "Β":"3",
+    "Γ":"12",
+    "Δ":"5"
+}
+
+
+game = classes.Game()
+game.run()
+sak = classes.SakClass(letters)
+
+
+
