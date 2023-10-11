6ca95b025ce8d4d9a5ba0c664ba45d4d5d4d3818
michailidm <72384900+michailidm@users.noreply.github.com>
2023-10-11 13:29:18 +0300

    Delete main-36082642.py

--- main-36082642.py
+++
@@ -1,8 +0,0 @@
-import classes
-
-sak = classes.SakClass()   # letters_weight, letters_amount)
-
-# human = classes.Human()
-# computer = classes.Computer()
-game = classes.Game()  #sak, human, computer)
-game.setup()
